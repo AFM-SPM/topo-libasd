6ac715b309d8f435c1f7bfcaf9580007b0ff5923
SylviaWhittle <sylviwhittle@gmail.com>
2023-09-07 15:59:52 +0100

    Undo attempt at fix

--- topo_libasd/topo_libasd.hpp
+++ topo_libasd/topo_libasd.hpp
@@ -1,8 +1,8 @@
-#ifndef TOPO_LIBASD_TOPO_LIBASD_ROOT_HEADER
-#define TOPO_LIBASD_TOPO_LIBASD_ROOT_HEADER
+#ifndef LIBASD_LIBASD_ROOT_HEADER
+#define LIBASD_LIBASD_ROOT_HEADER
 
 #include "data.hpp"
 #include "read_asd.hpp"
 #include "write_asd.hpp"
 
-#endif // LIBASD_LIBASD_ROOT_HEADER
+#endif// LIBASD_LIBASD_ROOT_HEADER
